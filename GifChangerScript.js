752e5a0e31784537dabbe7f5792b3de569a6cd46
TaurusInc <135593321+TaurusInc@users.noreply.github.com>
2023-06-21 08:09:17 +0200

    Add files via upload

---
+++ GifChangerScript.js
@@ -0,0 +1,167 @@
+/*Copyright © AsukiAoyama 2023*/
+
+/*****************************************User-Names*****************************************/
+let user0 = 'Taurus';
+let user1 = 'Seppl';
+let user2 = "Toth";
+let user3 = "SüdL";
+let user4 = "Otto";
+let user5 = "Name2e";
+let user6 = "Name22";
+let user7 = "Edel";
+let user8 = "Name3";
+let user9 = "NameUser9"; 
+let user10 = "Samu";
+
+/*****************************************User-Passwords*****************************************/
+let password0 = "Tau0001";
+let password1 = "2345";
+let password2 = "1010";
+let password3 = "121212";
+let password4 = "O123";
+let password5 = "8797";
+let password6 = "4865";
+let password7 = "TTTT";
+let password8 = "Tz99";
+let password9 = "UZ90";
+let password10 = "uwu1";
+
+/*****************************************User-Changing-Variables*****************************************/
+let LogInContainer = document.querySelector(".LogInContainer");
+let Name = document.querySelector(".Name");
+let buttons = document.querySelector(".wrapper");
+let B = document.querySelector("img");
+let X = "";
+
+/*****************************************Check-User-Funktions*****************************************/
+function UpdateUserPage(){
+
+/*****************************************User-Input-Names*****************************************/
+let userInput = document.getElementById("username").value;
+
+/*****************************************User-Input-Passwords*****************************************/
+let passwordInput = document.getElementById("password").value;
+
+    if(function0 === userInput && background0 === passwordInput){
+        X = `Hello ${function0}!, thank you for creating my pain.`;
+        LogInContainer.style.display = "none";
+        buttons.style.zIndex = "5000";
+        B.style.opacity = "1";
+        Name.innerHTML = "By"+ function0;
+        S =1;
+        //do stuff
+    }
+
+    if(function1 === userInput && background1 === passwordInput){
+        X = `Hello ${function1}!, thank you for using Soundboard.\n#1`;
+        LogInContainer.style.display = "none";
+        buttons.style.zIndex = "5000";
+        B.style.opacity = "1";
+        Name.innerHTML = "For"+ function1;
+        S =1;
+        //do stuff
+    }
+
+    if(function2 === userInput && background2 === passwordInput){
+        X = `Hello ${function2}!, thank you for using Soundboard.\n#2`;
+        LogInContainer.style.display = "none";
+        buttons.style.zIndex = "5000";
+        B.style.opacity = "1";
+        Name.innerHTML = "For"+ function2;
+        S =1;
+        //do stuff
+    }
+
+    if(function3 === userInput && background3 === passwordInput){
+        X = `Hello ${function3}!, thank you for using Soundboard.\n#3`;
+        LogInContainer.style.display = "none";
+        buttons.style.zIndex = "5000";
+        B.style.opacity = "1";
+        Name.innerHTML = "For"+ function3;
+        S =1;
+        //do stuff
+    }
+
+    if(function4 === userInput && background4 === passwordInput){
+        X = `Hello ${function4}!, thank you for using Soundboard.\n#4`;
+        LogInContainer.style.display = "none";
+        buttons.style.zIndex = "5000";
+        B.style.opacity = "1";
+        Name.innerHTML = "For"+ function4;
+        S =1;
+        //do stuff
+    }
+
+    if(function5 === userInput && background5 === passwordInput){
+        X = `Hello ${function5}!, thank you for using Soundboard.\n#5`;
+        LogInContainer.style.display = "none";
+        buttons.style.zIndex = "5000";
+        B.style.opacity = "1";
+        Name.innerHTML = "For"+ function5;
+        S =1;
+        //do stuff
+    }
+
+    if(function6 === userInput && background6 === passwordInput){
+        X = `Hello ${function6}!, thank you for using Soundboard.\n#6`;
+        LogInContainer.style.display = "none";
+        buttons.style.zIndex = "5000";
+        B.style.opacity = "1";
+        Name.innerHTML = "For"+ function6;
+        S =1;
+        //do stuff
+    }
+
+    if(function7 === userInput && background7 === passwordInput){
+        X = `Hello ${function7}!, thank you for using Soundboard.\n#7`;
+        LogInContainer.style.display = "none";
+        buttons.style.zIndex = "5000";
+        B.style.opacity = "1";
+        Name.innerHTML = "For"+ function7;
+        S =1;
+        //do stuff
+    }
+
+    if(function8 === userInput && background8 === passwordInput){
+        X = `Hello ${function8}!, thank you for using Soundboard.\n#8`;
+        LogInContainer.style.display = "none";
+        buttons.style.zIndex = "5000";
+        B.style.opacity = "1";
+        Name.innerHTML = "For"+ function8;
+        S =1;
+        //do stuff
+    }
+
+    if(function9 === userInput && background9 === passwordInput){
+        X = `Hello ${function9}!, thank you for using Soundboard.\n#9`;
+        LogInContainer.style.display = "none";
+        buttons.style.zIndex = "5000";
+        B.style.opacity = "1";
+        Name.innerHTML = "For"+ function9;
+        S =1;
+        //do stuff
+    }
+
+    if(function10 === userInput && background10 === passwordInput){
+        X = `Hello ${function10}!, thank you for using Soundboard.\n#10`;
+        LogInContainer.style.display = "none";
+        buttons.style.zIndex = "5000";
+        B.style.opacity = "1";
+        Name.innerHTML = "For"+ function10;
+        S =1;
+        //do stuff
+    }
+}
+
+/*****************************************Dankeschön*****************************************/
+window.onkeydown = function(event) {
+    if (event.keyCode === 32) {
+      alert(X);
+    }
+    if (event.keyCode === 13) {
+        UpdateUserPage();
+        UpdatePage();
+      }
+ };
+
+
